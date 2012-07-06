536b572e028cb41f239988f7534402ad7579f654
Rinat Silnov <rinat@clevertech.biz>
2012-07-06 20:00:31 +0400

    Upgraded some libraries

--- app/js/app.js
+++ app/js/app.js
@@ -1,138 +1,14 @@
 define([
   'jquery',
-  'underscore', 
+  'underscore',
   'backbone',
-  'domReady',
-  'router',
-  'models/login',
-  'models/post',
-  'models/comment',
-  'collections/user',
-  'collections/post',
-  'collections/comment',
-  'views/home',
-  'views/navbar',
-  'views/search',
-  'views/login',
-  'views/user/list',
-  'views/post/list',
-  'views/comment/list',
-  'views/post/form',
-  'views/comment/form',
-  'cookie',
-  'bootstrapDropdown',
-  'bootstrapModal',
-  'backboneRelational',
-  ], function($, _, Backbone, domReady,
-    Router, 
-    LoginModel, PostModel, CommentModel,
-    UserCollection, PostCollection, CommentCollection,
-    HomeView, NavbarView, SearchView, LoginView,
-    UserListView, PostListView, CommentListView, 
-    PostFormView, CommentFormView) {
+  'marionette'
+], function($, _, Backbone, Marionette) {
 
-  var App = Backbone.View.extend({
-
-    initialize: function() {
-      _.bindAll(this,'initSearch','initViews','initRouter','initMisc','checkAuth');
-
-      // declare main custom event object
-      this.vent = _.extend({}, Backbone.Events);
-
-      this.users    = new UserCollection;
-      this.users.fetch();
-
-      this.posts    = new PostCollection;
-      this.comments = new CommentCollection;
-
-      // Initialize login view 
-      this.loginModel = new LoginModel;
-
-      this.initViews();
-      this.initSearch();
-      this.initMisc();
-      this.initRouter();
-	  this.checkAuth();
-    },
-
-    checkAuth: function() {
-	  self = this;
-
-      // Check user authentication
-      var params = this.getCookieParams();
-      if (params) {
-        self.loginModel.set('username', params.username);
-        self.loginModel.set('token', params.token);
-        self.loginModel.set('authenticated', true);
-      } 
-    },
-
-    initSearch: function() {
-      // Initialize main search 
-      var self = this;
-      domReady(function() {
-        var searchView = new SearchView({
-          posts    : self.posts,
-          comments : self.comments,
-          vent     : self.vent,
-        }); 
-      });
-    },
-
-    initViews: function() {
-      // Login view
-      var loginView = new LoginView({model:this.loginModel, vent:this.vent});
-          loginView.render();
-
-      // Navbar view
-      var navbarView = new NavbarView({vent:this.vent});
-
-      // List views
-      var userListView    = new UserListView({collection:this.users, vent:this.vent});
-      var postListView = new PostListView({
-        collection : this.posts,
-        user       : this.loginModel,
-        vent       : this.vent,
-      });
-      var commentListView = new CommentListView({collection:this.comments, vent:this.vent});
-
-      // Form views
-      var postFormView = new PostFormView({
-        model : new PostModel,
-        user  : this.loginModel,
-        vent  : this.vent,
-      });
-      var commentFormView = new CommentFormView({model: new CommentModel, vent:this.vent});
-    },
-
-    initRelationalModels: function() {
-      // you can put dummy model initialization to fix a flaw in relations (new Model());
-
-    },
-
-    initRouter: function() {
-      // Initialize router
-      this.router = new Router({vent:this.vent});
-      Backbone.history.start();
-    },
-
-    initMisc: function() {
-      // JS sugar for all dropdowns with this class
-      $('.dropdown-toggle').dropdown();
-    },
-
-    getCookieParams: function() {
-      if ($.cookie('_yiibackbone')) {
-        var data = $.cookie('_yiibackbone').split(',');
-        var params = {
-          username: data[0],
-          token: data[1],
-        }
-        return params;
-      }
-    },
-
+  var App = new Marionette.Application;
+  App.addRegions({
+    mainRegion: '.main',
+    headRegion: '.head'
   });
-
-  return new App;
+  return App;
 });
--- app/js/main.js
+++ app/js/main.js
@@ -7,7 +7,7 @@ define.amd.jQuery = true;
 // Require.js allows us to configure mappings to paths
 // as demonstrated below:
 // TODO: Load minified version of the libs or use Require.js's JS compiler (R)
-require.config({
+requirejs.config({
   paths: {
     jquery               : 'libs/jquery/jquery-1.7.1.min',
     jqueryUICore         : 'libs/jquery-ui/jquery.ui.core',
@@ -33,9 +33,156 @@ require.config({
     bootstrapTab         : 'libs/bootstrap/bootstrap-tab',
     bootstrapTypeahead   : 'libs/bootstrap/bootstrap-typeahead',
     // bootstrapWysihtml5   : 'libs/bootstrap/bootstrap-wysihtml5',
+  },
+  shim: {
+    underscore: {
+      exports: '_'
+    },
+    backbone: {
+      deps: ["underscore", "jquery"],
+      exports: "Backbone"
+    },
+    visualsearch: {
+      deps: ['backbone', 'jqueryUIAutocomplete']
+    }
   }
 });
 
-require(['app'], function(App){
-  var app = App;
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'domReady',
+  'router',
+  'models/login',
+  'models/post',
+  'models/comment',
+  'collections/user',
+  'collections/post',
+  'collections/comment',
+  'views/home',
+  'views/navbar',
+  'views/search',
+  'views/login',
+  'views/user/list',
+  'views/post/list',
+  'views/comment/list',
+  'views/post/form',
+  'views/comment/form',
+  'cookie',
+  'bootstrapDropdown',
+  'bootstrapModal',
+  'backboneRelational'
+], function($, _, Backbone, domReady,
+            Router,
+            LoginModel, PostModel, CommentModel,
+            UserCollection, PostCollection, CommentCollection,
+            HomeView, NavbarView, SearchView, LoginView,
+            UserListView, PostListView, CommentListView,
+            PostFormView, CommentFormView) {
+
+  var App = Backbone.View.extend({
+
+    initialize: function() {
+      _.bindAll(this,'initSearch','initViews','initRouter','initMisc','checkAuth');
+
+      // declare main custom event object
+      this.vent = _.extend({}, Backbone.Events);
+
+      this.users    = new UserCollection;
+      this.users.fetch();
+
+      this.posts    = new PostCollection;
+      this.comments = new CommentCollection;
+
+      // Initialize login view
+      this.loginModel = new LoginModel;
+
+      this.initViews();
+      this.initSearch();
+      this.initMisc();
+      this.initRouter();
+      this.checkAuth();
+    },
+
+    checkAuth: function() {
+      var self = this;
+
+      // Check user authentication
+      var params = this.getCookieParams();
+      if (params) {
+        self.loginModel.set('username', params.username);
+        self.loginModel.set('token', params.token);
+        self.loginModel.set('authenticated', true);
+      }
+    },
+
+    initSearch: function() {
+      // Initialize main search
+      var self = this;
+      domReady(function() {
+        var searchView = new SearchView({
+          posts    : self.posts,
+          comments : self.comments,
+          vent     : self.vent,
+        });
+      });
+    },
+
+    initViews: function() {
+      // Login view
+      var loginView = new LoginView({model:this.loginModel, vent:this.vent});
+      loginView.render();
+
+      // Navbar view
+      var navbarView = new NavbarView({vent:this.vent});
+
+      // List views
+      var userListView    = new UserListView({collection:this.users, vent:this.vent});
+      var postListView = new PostListView({
+        collection : this.posts,
+        user       : this.loginModel,
+        vent       : this.vent,
+      });
+      var commentListView = new CommentListView({collection:this.comments, vent:this.vent});
+
+      // Form views
+      var postFormView = new PostFormView({
+        model : new PostModel,
+        user  : this.loginModel,
+        vent  : this.vent,
+      });
+      var commentFormView = new CommentFormView({model: new CommentModel, vent:this.vent});
+    },
+
+    initRelationalModels: function() {
+      // you can put dummy model initialization to fix a flaw in relations (new Model());
+
+    },
+
+    initRouter: function() {
+      // Initialize router
+      this.router = new Router({vent:this.vent});
+      Backbone.history.start();
+    },
+
+    initMisc: function() {
+      // JS sugar for all dropdowns with this class
+      $('.dropdown-toggle').dropdown();
+    },
+
+    getCookieParams: function() {
+      if ($.cookie('_yiibackbone')) {
+        var data = $.cookie('_yiibackbone').split(',');
+        var params = {
+          username: data[0],
+          token: data[1],
+        }
+        return params;
+      }
+    },
+
+  });
+
+  return new App;
 });
