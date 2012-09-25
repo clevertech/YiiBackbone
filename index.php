7379bbeb55a63a676ebe635e360c816f471c3aa0
Rinat Silnov <rinat@clevertech.biz>
2012-09-25 20:19:50 +0400

    Yii moved into submodule

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/protected/lib/yii-1.1.9.r3527/framework/yii.php';
+$yii=dirname(__FILE__).'/protected/vendor/yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
